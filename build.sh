52796137b5fa5ab3de8447c08be47ea6ea27d8a2
ngdream <96875506+ngdream@users.noreply.github.com>
2023-06-02 17:58:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ mkdir build/modules/
 cp ./src/mainprocess/ui/ressources/* ./build/ui/ressources
 g++ -std=c++11 ./src/mainprocess/extensions/javascriptextensions.cpp -shared -fPIC -o ./build/modules/javascriptextensions.so `pkg-config webkit2gtk-web-extension-4.0 --libs --cflags`
 g++ -std=c++17 ./src/mainprocess/main.cpp `pkg-config gtkmm-3.0 --libs --cflags` `pkg-config webkit2gtk-4.0 --libs --cflags` -o build/quizy.ng
-npm run build
+npm run rbuild
