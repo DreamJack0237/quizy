f91bf7028f44d28f57fcdbd09deafeced291ccf3
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-06-07 00:42:10 +0100

    interface 1.0.1

--- src/composants/Qcm.jsx
+++ src/composants/Qcm.jsx
@@ -8,7 +8,7 @@ const Qcm = () => {
             <Header />
             <div className='justify'>
 
-                <div >
+                <div className='flex col'>
                     <div className='Question'>
                         <p className='Questionn_umber'>1.</p>
                         <h1 className='Question_text'>
@@ -65,68 +65,25 @@ const Qcm = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div>
-                    <div className='Question'>
-                        <p className='Questionn_umber'>2.</p>
-                        <h1 className='Question_text'>
-                            Combien de charactaire une 'URL' peut elle contenir ?
-                        </h1>
-                    </div>
-
-                    <div className='main_options'>
-                        <div className='options'>
-                            <label className="switch">
-                                <input type="checkbox" />
-                                <span className="slider"><p>A.</p></span>
-                            </label>
-                            <div>
-                                <p>
-                                    2080
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className='options'>
-                            <label className="switch">
-                                <input type="checkbox" />
-                                <span className="slider"><p>B.</p></span>
-                            </label>
-                            <div>
-                                <p>
-                                    2080
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className='options'>
-                            <label className="switch">
-                                <input type="checkbox" />
-                                <span className="slider"><p>C.</p></span>
-                            </label>
-                            <div>
-                                <p>
-                                    20000
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className='options'>
-                            <label className="switch">
-                                <input type="checkbox" />
-                                <span className="slider"><p>D.</p></span>
-                            </label>
-                            <div>
-                                <p>
-                                    1080
-                                </p>
-                            </div>
-                        </div>
+                    <div className='Asubmit flex'>
+                        <button className='submitbtn'>
+                            Submit
+                        </button>
                     </div>
                 </div>
 
-                <Time/>
+                
+
+                <div className='flex spbt pad10 control'>
+
+                    <Time />
+
+                    <button className="NextPage">
+                        Next page &#65125;
+                    </button>
+                    
+                </div>
+
 
             </div>
 
